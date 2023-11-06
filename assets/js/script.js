ea64d83d6ee9e81afc0ce9e98c737d27883652e6
donovanainsley <donovan_weise@live.co.uk>
2023-11-06 20:28:40 +0000

    add code to increment correct score and incorrect answer.

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,7 +38,6 @@ function runGame(gameType) {
  * Checks the answer against the first element in
  * the returned calculateCorrectAnswer array.
  */
-
 function checkAnswer() {
     let userAnswer = parseInt(document.getElementById("answer-box").value);
     let calculatedAnswer = calculateCorrectAnswer();
@@ -46,19 +45,20 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got is correct!");
+        incrementScore();
     } else {
         alert(`D'oh... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
-    
+
 }
 
 /**
  * Gets the operands (the numbers) and the operator (plus, minus, etc)
  * directly from the DOM, and returns the correct answer.
  */
-
 function calculateCorrectAnswer() {
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand2').innerText);
@@ -72,12 +72,20 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Get's the current score from the DOM and increments it by 1.
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Get's the current incorrect score from the DOM and increments it by 1.
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2){
